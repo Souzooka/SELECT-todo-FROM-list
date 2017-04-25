45a41543297f38291bd2b699c12e0e3326aa1904
Dakota <bazookaharwell@gmail.com>
2017-04-24 18:41:23 -1000

    Changed update_at properties

--- todo_app.sql
+++ todo_app.sql
@@ -1,16 +1,44 @@
-CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
--- DROP USER IF EXISTS michael;
--- CREATE USER michael WITH ENCRYPTED PASSWORD 'stonebreaker';
+--Create user michael
+DROP USER IF EXISTS michael;
+CREATE USER michael WITH ENCRYPTED PASSWORD 'stonebreaker';
 
--- DROP DATABASE IF EXISTS todo_app;
--- CREATE DATABASE todo_app;
+--Create database todo_app
+DROP DATABASE IF EXISTS todo_app;
+CREATE DATABASE todo_app;
 
+--Create table tasks
 DROP TABLE IF EXISTS tasks;
 CREATE TABLE tasks (
   id SERIAL PRIMARY KEY,
   title VARCHAR(255) NOT NULL,
   description TEXT NULL,
-  created_at TIMESTAMP NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT NOW(),
   updated_at TIMESTAMP NULL,
   completed BOOLEAN NOT NULL);
+
+--Delete completed column from tasks
+ALTER TABLE tasks
+DROP completed;
+
+--Add completed_at column to tasks
+ALTER TABLE tasks
+ADD completed_at TIMESTAMP NULL
+DEFAULT NULL;
+
+--Set any existing tasks updated_at which are null to 0
+UPDATE tasks
+SET updated_at = NOW()
+WHERE updated_at IS NULL;
+
+--Disable saving as null in updated_at
+ALTER TABLE tasks
+ALTER COLUMN updated_at SET NOT NULL;
+
+--Drop default for tasks
+ALTER TABLE tasks
+ALTER COLUMN updated_at DROP DEFAULT;
+
+--Change default for updated_at
+ALTER TABLE tasks
+ALTER COLUMN updated_at SET DEFAULT NOW();
